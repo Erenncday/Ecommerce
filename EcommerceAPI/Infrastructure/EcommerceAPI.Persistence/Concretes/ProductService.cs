a5b36bc75d119187412ca04dd4941c6a3ff0a97c
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2023-11-30 14:05:32 +0300

    Initial commit

---
+++ EcommerceAPI/Infrastructure/EcommerceAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,18 @@
+﻿using EcommerceAPI.Application.Abstractions;
+using EcommerceAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
