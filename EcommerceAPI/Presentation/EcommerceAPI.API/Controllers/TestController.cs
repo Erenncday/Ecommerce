b79d937b8d5497982056dd282fe71eacca3ded24
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2024-01-03 17:25:53 +0300

    Tracking Performans Optimizasyonu yapıldı

--- EcommerceAPI/Presentation/EcommerceAPI.API/Controllers/TestController.cs
+++ EcommerceAPI/Presentation/EcommerceAPI.API/Controllers/TestController.cs
@@ -17,17 +17,36 @@ namespace EcommerceAPI.API.Controllers
 			_productWriteRepository = productWriteRepository;
 		}
 
+		//[HttpGet]
+		//public async Task Get()
+		//{
+		//	await _productWriteRepository.AddRangeAsync(new()
+		//	{
+		//		new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock=100},
+		//		new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock=200},
+		//		new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock=300},
+		//	});
+
+		//	var count = await _productWriteRepository.SaveAsync();
+		//}
+
 		[HttpGet]
 		public async Task Get()
 		{
-			await _productWriteRepository.AddRangeAsync(new()
-			{
-				new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 100, CreatedDate = DateTime.UtcNow, Stock=100},
-				new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 200, CreatedDate = DateTime.UtcNow, Stock=200},
-				new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 300, CreatedDate = DateTime.UtcNow, Stock=300},
-			});
+			//Product p = await _productReadRepository.GetByIdAsync("2dc23820-5a44-4d26-85a6-6e2ca8793e86");
+			//p.Name = "Telefon";
+			//await _productWriteRepository.SaveAsync();
 
-			var count = await _productWriteRepository.SaveAsync();
+			Product prod = await _productReadRepository.GetByIdAsync("2dc23820-5a44-4d26-85a6-6e2ca8793e86", false);
+			prod.Name = "Eren";
+			await _productWriteRepository.SaveAsync();
+
+
+			//Product pr = await _productReadRepository.GetByIdAsync("7158ba44-5152-4df1-9ab6-14909c9d9c9d");
+			//pr.Name = "Bilgisayar";
+			//await _productWriteRepository.SaveAsync();
+
+
 		}
 
 		[HttpGet("{id}")]
