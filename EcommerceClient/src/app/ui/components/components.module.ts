27bb6327fe7a587de1f0789dbfa82d3d4a548bb4
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2024-01-05 17:31:20 +0300

    Admin ve UI arayüz altyapısı geliştirildi

---
+++ EcommerceClient/src/app/ui/components/components.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsComponent } from './products/products.component';
+import { BasketsComponent } from './baskets/baskets.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsModule } from './products/products.module';
+import { BasketsModule } from './baskets/baskets.module';
+import { HomeModule } from './home/home.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductsModule,
+    BasketsModule,
+    HomeModule
+  ]
+})
+export class ComponentsModule { }
