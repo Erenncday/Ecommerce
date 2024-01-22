e6f83a9d8c5c526d30f7ea7fd5e3e39d957cd547
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2024-01-22 10:35:48 +0300

    AlertifyJs kütüphane yüklendi ve customize edildi.

--- EcommerceClient/src/app/app.component.ts
+++ EcommerceClient/src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   title = 'EcommerceClient';
 }
 
-$(document).ready(() =>
-{
-  alert("Hoşgeldiniz")
-})
+// $(document).ready(() =>
+// {
+//   alert("Hoşgeldiniz")
+// })
