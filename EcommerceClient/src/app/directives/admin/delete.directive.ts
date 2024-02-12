963d90fe9a4e53d9176654f58ce1090db469e064
Eren İncedayı <103766421+Erenncday@users.noreply.github.com>
2024-02-12 21:20:43 +0300

    Delete Directive alanındaki hatalar giderildi. Yüklenme animasyonları düzenlendi

--- EcommerceClient/src/app/directives/admin/delete.directive.ts
+++ EcommerceClient/src/app/directives/admin/delete.directive.ts
@@ -1,9 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
-import { ProductService } from 'src/app/services/common/models/product.service';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 declare var $ : any;
 
@@ -12,7 +14,7 @@ declare var $ : any;
 })
 export class DeleteDirective {
 
-  constructor(private spinner : NgxSpinnerService , private element : ElementRef, private _renderer : Renderer2, private productService : ProductService, public dialog: MatDialog) 
+  constructor(private spinner : NgxSpinnerService , private element : ElementRef, private _renderer : Renderer2, private httpClientService : HttpClientService, public dialog: MatDialog, private alertifyService : AlertifyService) 
   { 
     const img = _renderer.createElement("img");
 
@@ -25,6 +27,7 @@ export class DeleteDirective {
   }
 
   @Input() id : string;
+  @Input() controller : string;
   @Output() callback : EventEmitter<any> = new EventEmitter();
 
   @HostListener("click")
@@ -37,15 +40,33 @@ export class DeleteDirective {
       
       const td : HTMLTableCellElement = this.element.nativeElement;
 
-      await this.productService.delete(this.id);
-  
-      $(td.parentElement).fadeOut(2000, () => 
-      {
-        this.callback.emit();
-      });
+      await this.httpClientService.delete(
+        {
+          controller : this.controller
+        }, this.id).subscribe(data => 
+          {
+            $(td.parentElement).fadeOut(2000, () => 
+            {
+              this.callback.emit();
+              this.alertifyService.message("Ürün başarıyla silindi.", {
+                dismissOthers : true,
+                messageType : MessageType.Success,
+                position : Position.TopRight
+              })
+            });
+          }, (errorResponse : HttpErrorResponse) =>
+          {
+
+              this.spinner.hide(SpinnerType.Timer)
+
+              this.alertifyService.message("Silme işlemi gerçekleştirilirken beklenmeyen bir hayatla karşılaşıldı!", {
+              dismissOthers : true,
+              messageType : MessageType.Error,
+              position : Position.TopRight
+            })
+          });
     });
   }
-
 
   openDialog(afterClosed : any): void {
     const dialogRef = this.dialog.open(DeleteDialogComponent, {
